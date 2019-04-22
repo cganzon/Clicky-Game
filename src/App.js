2a236f162cfebbd0c0dd5b8ae85f3d6611468899
cganzon <crganzon@gmail.com>
2019-04-22 15:52:34 -0700

    score functionality

--- src/App.js
+++ src/App.js
@@ -7,15 +7,42 @@ import friends from "./friends.json";
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    friends: friends,
+    score: 0,
+    highScore: 0,
+    overallHighScore: "",
+    clicked: []
   };
 
   clickedImage = id => {
-    alert(id);
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    // const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    // this.setState({ friends });
+    // alert(id);
+    // If the ID of the image is already in the clicked array, reset the game
+      if(this.state.clicked.includes(id)) {
+        alert("You clicked this image already!");
+        this.setState({score: 0});
+        // If the game high score is greater than the current highest score, set the game high score to be the new highest score, otherwise keep the highest score the same
+        if(this.state.highScore > this.state.overallHighScore) {
+          this.setState({overallHighScore: this.state.highScore});
+        } else {
+          this.setState({overallHighScore: this.state.overallHighScore});
+        }
+        this.setState({clicked: []});
+        this.setState({highScore: 0});
+        console.log(this.state);
+      } 
+      // Otherwise, push the image's ID into the clicked array and increment the scores
+      else {
+        let score = this.state.score;
+        score++;
+        this.setState({score: score});
+        let highScore = this.state.highScore;
+        highScore++;   
+        this.setState({highScore: highScore});
+        let clicked = this.state.clicked;
+        clicked.push(id);
+        this.setState({clicked: clicked});
+        console.log(this.state);
+      }
   };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
@@ -23,17 +50,19 @@ class App extends Component {
     return (
       <Wrapper>
         <Title>Clicky Game!</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            clickedImage={this.clickedImage}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
+        <h2>Score: {this.state.score}</h2>
+        <h2>High Score: {this.state.overallHighScore}</h2>
+          {this.state.friends.map(friend => (
+            <FriendCard
+              clickedImage={this.clickedImage}
+              id={friend.id}
+              key={friend.id}
+              name={friend.name}
+              image={friend.image}
+              occupation={friend.occupation}
+              location={friend.location}
+            />
+          ))}
       </Wrapper>
     );
   }
--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -5,7 +5,7 @@ function FriendCard(props) {
   return (
     <div className="card">
       <div className="img-container">
-        <img alt={props.name} src={props.image} onClick={() => props.clickedImage(props.id)}/>
+        <img className="img-thumbnail" alt={props.name} src={props.image} onClick={() => props.clickedImage(props.id)}/>
       </div>
 
     </div>

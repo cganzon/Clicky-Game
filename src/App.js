f5be910e1a89641db906b863e6461b610126331b
cganzon <crganzon@gmail.com>
2019-04-22 17:44:21 -0700

    added comments

--- src/App.js
+++ src/App.js
@@ -17,9 +17,10 @@ class App extends Component {
 
   clickedImage = id => {
     // alert(id);
-    // If the ID of the image is already in the clicked array, reset the game
+    // If the ID of the image is already in the clicked array, reset the game and display current highest score
     if (this.state.clicked.includes(id)) {
-      alert("Sorry you clicked this image alreaady!");
+      alert("Sorry you clicked this image already!");
+
       this.setState({ score: 0 });
       // If the game high score is greater than the current highest score, set the game high score to be the new highest score, otherwise keep the highest score the same
       if (this.state.highScore > this.state.overallHighScore) {
@@ -48,22 +49,22 @@ class App extends Component {
     }
   };
 
-  // Map over this.state.heroes and render a FriendCard component for each friend object
+  // Map over this.state.heroes and render a HeroCard component for each hero object
   render() {
     return (
       <Wrapper>
         <Title>Overwatch Clicky Game!</Title>
         <Title>Score: {this.state.score}</Title>
         <Title>High Score: {this.state.overallHighScore}</Title>
-        {this.state.heroes.map(friend => (
+        {this.state.heroes.map(hero => (
           <HeroCard
             clickedImage={this.clickedImage}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
+            id={hero.id}
+            key={hero.id}
+            name={hero.name}
+            image={hero.image}
+            occupation={hero.occupation}
+            location={hero.location}
           />
         ))}
       </Wrapper>
